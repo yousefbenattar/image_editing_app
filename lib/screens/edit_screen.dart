5a46ae947173fc40051e51f9084a22c49a2ccc4c
Youssef Ben Attar <youssefdisvitas@gmail.com>
2024-03-29 02:42:21 +0000

    app done

--- lib/screens/edit_screen.dart
+++ lib/screens/edit_screen.dart
@@ -1,31 +1,120 @@
 import 'dart:io';
-
 import 'package:flutter/material.dart';
-
+import 'package:image_editing_app/widgets/image_text.dart';
+import 'package:screenshot/screenshot.dart';
 import '../widgets/edit_view_model.dart';
-
+const SizedBox box =  SizedBox(width: 5,);
 class EditScreen extends StatefulWidget {
-  const EditScreen({super.key,required this.selectedImage});
+  const EditScreen({super.key, required this.selectedImage});
   final String selectedImage;
 
   @override
   State<EditScreen> createState() => _EditScreenState();
+  
 }
 
 class _EditScreenState extends EditImageViewModel {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Image.file(File(widget.selectedImage)),
-      floatingActionButton: floatingbtn(),
+      appBar: _appBar,
+      body: Screenshot(
+        controller: screenshotController,
+        child: SafeArea(
+            child: SizedBox(
+          height: MediaQuery.of(context).size.height * 0.3,
+          child: Stack(
+            children: [
+              _selectedImage,
+              for (int i = 0; i < texts.length; i++)
+                Positioned(
+                    top: texts[i].top,
+                    left: texts[i].left,
+                    child: GestureDetector(
+                      onLongPress: () => print('on long press'),
+                      onTap: () => setCurrentIndex(context,i),
+                      child: Draggable(
+                        feedback: ImageText(textInfo: texts[i]),
+                        onDragEnd: (drag) {
+                          final renderBox =
+                              context.findRenderObject() as RenderBox;
+                          Offset off = renderBox.globalToLocal(drag.offset);
+                          setState(() {
+                            texts[i].top = off.dy -96;
+                            texts[i].left = off.dx;
+                          });
+                        },
+                        child: ImageText(textInfo: texts[i]),
+                      ),
+                    )),
+              creatorText.text.isNotEmpty
+                  ? Positioned(
+                      left: 0,
+                      bottom: 0,
+                      child: Text(
+                        creatorText.text,
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black.withOpacity(0.3)),
+                      ))
+                  : const SizedBox.shrink()
+            ],
+          ),
+        )),
+      ),
+      floatingActionButton: _floatingbtn,
     );
   }
-  Widget floatingbtn () =>
-  FloatingActionButton(onPressed: () => addNewDailog(context),
-  tooltip: '',
-  backgroundColor: Colors.blue,
-  child:const Icon(Icons.edit,color: Colors.white,),
-  );
+
+  Widget get _selectedImage => Center(
+        child: Image.file(
+          File(widget.selectedImage),
+          fit: BoxFit.fill,
+        ),
+      );
+  Widget get _floatingbtn => FloatingActionButton(
+        onPressed: () => addNewDailog(context),
+        tooltip: '',
+        backgroundColor: Colors.blue,
+        child: const Icon(
+          Icons.edit,
+          color: Colors.white,
+        ),
+      );
+
+AppBar get _appBar => AppBar(
   
+      backgroundColor: Colors.white,
+      automaticallyImplyLeading: false,
+      title: SizedBox(
+        height: 50,
+        child: ListView(
+          scrollDirection: Axis.horizontal,
+          children: [
+            IconButton(tooltip: 'Save Image',onPressed: ()=>saveToGallery(context), icon:const Icon(Icons.save,color: Colors.black,)),
+            IconButton(tooltip: 'Delete text',onPressed: (){deletetext();}, icon:const Icon(Icons.delete,color: Colors.black,)),
+            IconButton(tooltip: 'Increase font size',onPressed: ()=>increaseFontSize(), icon:const Icon(Icons.add,color: Colors.black,)),
+            IconButton(tooltip: 'Decrease font size',onPressed: ()=>decreaseFontSize(), icon:const Icon(Icons.remove,color: Colors.black,)),
+            IconButton(tooltip: 'Align left',onPressed:alignLeft, icon:const Icon(Icons.format_align_left,color: Colors.black,)),
+            IconButton(tooltip: 'Align Center',onPressed:alignCenter, icon:const Icon(Icons.format_align_center,color: Colors.black,)),
+            IconButton(tooltip: 'Align Right',onPressed:alignRight, icon:const Icon(Icons.format_align_right,color: Colors.black,)),
+            IconButton(tooltip: 'Bold',onPressed: () => boldText(), icon:const Icon(Icons.format_bold,color: Colors.black,)),
+            IconButton(tooltip: 'Italic',onPressed: () =>italictext(), icon:const Icon(Icons.format_italic,color: Colors.black,)),
+            IconButton(tooltip: 'Add New Line',onPressed: ()=>addLinesToText (), icon:const Icon(Icons.space_bar,color: Colors.black,)),
+            Tooltip(message: 'Red',child: GestureDetector(onTap: ()=>changeTextColor(Colors.red) ,child: const CircleAvatar(backgroundColor: Colors.red,)),),box,
+            Tooltip(message: 'white',child: GestureDetector(onTap: () => changeTextColor(Colors.white),child: CircleAvatar(radius: 21,backgroundColor: Colors.black,child: const CircleAvatar(backgroundColor: Colors.white,))),),box,
+            Tooltip(message: 'Black',child: GestureDetector(onTap: () => changeTextColor(Colors.black),child: const CircleAvatar(backgroundColor: Colors.black,)),),box,
+            Tooltip(message: 'Blue',child: GestureDetector(onTap: () => changeTextColor(Colors.blue),child: const CircleAvatar(backgroundColor: Colors.blue,)),),box,
+            Tooltip(message: 'Yellow',child: GestureDetector(onTap: () => changeTextColor(Colors.yellow),child: const CircleAvatar(backgroundColor: Colors.yellow,)),),box,
+            Tooltip(message: 'Green',child: GestureDetector(onTap: () => changeTextColor(Colors.green),child: const CircleAvatar(backgroundColor: Colors.green,)),),box,
+            Tooltip(message: 'Orange',child: GestureDetector(onTap: () => changeTextColor(Colors.orange),child: const CircleAvatar(backgroundColor: Colors.orange,)),),box,
+            Tooltip(message: 'Pink',child: GestureDetector(onTap: () => changeTextColor(Colors.pink),child: const CircleAvatar(backgroundColor: Colors.pink,)),),box,
+          ],
+        ),
+      ),
+    );
+    
 }
 
--- lib/widgets/edit_view_model.dart
+++ lib/widgets/edit_view_model.dart
@@ -1,12 +1,128 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 import 'package:image_editing_app/model/text_info.dart';
+import 'package:image_editing_app/utiles/utils.dart';
 import 'package:image_editing_app/widgets/default_btn.dart';
+import 'package:image_gallery_saver/image_gallery_saver.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:screenshot/screenshot.dart';
 
 import '../screens/edit_screen.dart';
 
 abstract class EditImageViewModel extends State<EditScreen> {
+  ScreenshotController screenshotController = ScreenshotController();
   TextEditingController textEditingController = TextEditingController();
-  List<TextInfo> textlist =[];
+  TextEditingController creatorText = TextEditingController();
+  List<TextInfo> texts=[];
+  int currentIndex = 0 ;
+  saveToGallery(BuildContext context){
+    if(texts.isNotEmpty){
+      screenshotController.capture().then((Uint8List ? image) => saveImage(image!)).catchError((error)=> print(error.toString()));
+    }
+  }
+  saveImage(Uint8List bytes) async {
+    final time = DateTime.now().toIso8601String().replaceAll('.', '-').replaceAll(':', '-');
+    final name = "screenshot_$time";
+    await requestPermission(Permission.storage);
+    await ImageGallerySaver.saveImage(bytes,name: name);
+  }
+  deletetext(){
+    setState(() {
+      texts.removeAt(currentIndex);
+    });
+     ScaffoldMessenger.of(context).showSnackBar(
+     const SnackBar(content: Text('Text Deleted',style: TextStyle(fontSize: 16),))
+    );
+  }
+  setCurrentIndex(BuildContext context,index){
+    setState(() {currentIndex = index;});
+    ScaffoldMessenger.of(context).showSnackBar(
+     const SnackBar(content: Text('Selected For Styling',style: TextStyle(fontSize: 16),))
+    );
+  }
+  addLinesToText (){
+    setState(() {
+      if(texts[currentIndex].text.contains('\n'))
+      {
+       texts[currentIndex].text = texts[currentIndex].text.replaceAll('\n', ' ');
+      }
+      else{
+      texts[currentIndex].text =  texts[currentIndex].text.replaceAll(' ', '\n');
+      }
+    });
+  }
+  increaseFontSize() {
+    setState(() {
+      texts[currentIndex].fontSize += 2;
+    });
+  }
+
+  decreaseFontSize() {
+    setState(() {
+      texts[currentIndex].fontSize -= 2;
+    });
+  }
+
+  alignCenter() {
+    setState(() {
+      texts[currentIndex].textAlign = TextAlign.center;
+    });
+  }
+
+  alignRight() {
+    setState(() {
+      texts[currentIndex].textAlign = TextAlign.right;
+    });
+  }
+
+  alignLeft() {
+    setState(() {
+      texts[currentIndex].textAlign = TextAlign.left;
+    });
+  }
+
+  changeTextColor(Color color) {
+    setState(() {
+      texts[currentIndex].color = color;
+    });
+  }
+
+  boldText() {
+    setState(() {
+      if (texts[currentIndex].fontWeight == FontWeight.bold) {
+        texts[currentIndex].fontWeight = FontWeight.normal;
+      } else {
+        texts[currentIndex].fontWeight = FontWeight.bold;
+      }
+    });
+  }
+
+  italictext() {
+    setState(() {
+      if (texts[currentIndex].fontStyle == FontStyle.italic) {
+        texts[currentIndex].fontStyle = FontStyle.normal;
+      } else {
+        texts[currentIndex].fontStyle = FontStyle.italic;
+      }
+    });
+  }
+   addNewText(BuildContext context){
+    setState(() {
+      texts.add(TextInfo(
+      text: textEditingController.text,
+      left: 0,
+      top: 0,
+      color:Colors.black,
+      fontWeight: FontWeight.normal,
+      fontStyle: FontStyle.normal,
+      fontSize: 20,
+      textAlign: TextAlign.left
+      ));
+      Navigator.of(context).pop();
+      textEditingController.clear();
+    });
+  }
   addNewDailog(context) {
     showDialog(
         context: context,
@@ -23,28 +139,12 @@ abstract class EditImageViewModel extends State<EditScreen> {
         DefaultButton(onPressed:() =>Navigator.of(context).pop(),
         color: Colors.black,
         textcolor: Colors.white,
-        child:const Text('Back')),
+        child:const Text('Back',style: TextStyle(color: Colors.white),)),
         DefaultButton(onPressed:() =>addNewText(context),
-        color: Colors.red,
-        textcolor: Colors.black,
-        child:const Text('Add Text')),
+        color: Colors.blue,
+        textcolor: Colors.white,
+        child:const Text('Add Text',style: TextStyle(color: Colors.white),)),
         ],
    ));
-  }
-  
-  addNewText(context){
-    setState(() {
-      textlist.add(TextInfo(
-      text: textEditingController.text,
-      left: 0,
-      top: 0,
-      color:Colors.black,
-      fontWeight: FontWeight.normal,
-      fontStyle: FontStyle.normal,
-      fontSize: 20,
-      textAlign: TextAlign.left
-      ));
-      Navigator.of(context).pop();
-    });
   }
 }
